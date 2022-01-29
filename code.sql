c0c6e8a74de26b8cbdbed827c5d8cef79a236b42
Hammad1007 <76726810+Hammad1007@users.noreply.github.com>
2022-01-29 16:44:35 +0500

    create code.sql

---
+++ code.sql
@@ -0,0 +1,184 @@
+use master
+go 
+Create database ATM
+go 
+use ATM
+go
+
+create table [UserType](
+[userTypeId] int primary key,
+[name] varchar(20) not null
+)
+go
+create table [User](
+[userId] int primary key,
+[name] varchar(20) not null,
+[userType] int foreign key references UserType([userTypeId]),
+[phoneNum] varchar(15) not null,
+[city] varchar(20) not null
+)
+go
+create table CardType(
+[cardTypeID] int primary key,
+[name] varchar(15),
+[description] varchar(40) null
+)
+go
+create Table [Card](
+cardNum Varchar(20) primary key,
+cardTypeID int foreign key references  CardType([cardTypeID]),
+PIN varchar(4) not null,
+[expireDate] date not null,
+balance float not null
+)
+go
+
+Create table UserCard(
+userID int foreign key references [User]([userId]),
+cardNum varchar(20) foreign key references [Card](cardNum),
+primary key(cardNum)
+)
+go
+
+create table TransactionType(
+[transTypeID] int primary key,
+[typeName] varchar(15),
+[description] varchar(40) null
+)
+
+go
+
+create table [Transaction](
+transId int primary key,
+transDate date not null,
+cardNum varchar(20) foreign key references [Card](cardNum),
+amount int not null,
+transType int foreign key references TransactionType(transTypeID)
+)
+go
+
+INSERT [dbo].[UserType] ([userTypeId], [name]) VALUES (1, N'Silver')
+GO
+INSERT [dbo].[UserType] ([userTypeId], [name]) VALUES (2, N'Gold')
+GO
+INSERT [dbo].[UserType] ([userTypeId], [name]) VALUES (3, N'Bronze')
+GO
+INSERT [dbo].[UserType] ([userTypeId], [name]) VALUES (4, N'Common')
+GO
+
+INSERT [dbo].[User] ([userId], [name], [userType],[phoneNum], [city]) VALUES (1, N'Ali',2, N'03036067000', N'Narowal')
+GO
+INSERT [dbo].[User] ([userId], [name],  [userType],[phoneNum], [city]) VALUES (2, N'Ahmed',1, N'03036047000', N'Lahore')
+GO
+INSERT [dbo].[User] ([userId], [name], [userType], [phoneNum], [city]) VALUES (3, N'Aqeel',3, N'03036063000', N'Karachi')
+GO
+INSERT [dbo].[User] ([userId], [name], [userType], [phoneNum], [city]) VALUES (4, N'Usman',4,  N'03036062000', N'Sialkot')
+GO
+INSERT [dbo].[User] ([userId], [name], [userType], [phoneNum], [city]) VALUES (5, N'Hafeez',2, N'03036061000', N'Lahore')
+GO
+
+INSERT [dbo].[CardType] ([cardTypeID], [name], [description]) VALUES (1, N'Debit', N'Spend Now, Pay Now')
+GO
+INSERT [dbo].[CardType] ([cardTypeID], [name], [description]) VALUES (2, N'Credit', N'Spend Now, Pay later')
+GO
+INSERT [dbo].[CardType] ([cardTypeID], [name], [description]) VALUES (3, N'Gift', N'Enjoy')
+GO
+
+INSERT [dbo].[Card] ([cardNum], [cardTypeID], [PIN], [expireDate], [balance]) VALUES (N'6569', 3, N'1770', CAST(N'2022-07-01' AS Date), 43025.31)
+GO
+INSERT [dbo].[Card] ([cardNum], [cardTypeID], [PIN], [expireDate], [balance]) VALUES (N'3336', 3, N'0234', CAST(N'2020-03-02' AS Date), 14425.62)
+GO
+INSERT [dbo].[Card] ([cardNum], [cardTypeID], [PIN], [expireDate], [balance]) VALUES (N'6566', 1, N'1234', CAST(N'2019-02-06' AS Date), 34325.52)
+GO
+INSERT [dbo].[Card] ([cardNum], [cardTypeID], [PIN], [expireDate], [balance]) VALUES (N'6456', 2, N'1200', CAST(N'2021-02-05' AS Date), 24325.3)
+GO
+INSERT [dbo].[Card] ([cardNum], [cardTypeID], [PIN], [expireDate], [balance]) VALUES (N'3436', 2, N'0034', CAST(N'2020-09-02' AS Date), 34025.12)
+GO
+
+INSERT [dbo].[UserCard] ([userID], [cardNum]) VALUES (1, N'6569')
+GO
+INSERT [dbo].[UserCard] ([userID], [cardNum]) VALUES (2, N'3336')
+GO
+INSERT [dbo].[UserCard] ([userID], [cardNum]) VALUES (3, N'6566')
+GO
+INSERT [dbo].[UserCard] ([userID], [cardNum]) VALUES (1, N'3436')
+GO
+
+INSERT [dbo].[TransactionType] ([transTypeID], [typeName]) VALUES (1, N'Withdraw')
+GO
+INSERT [dbo].[TransactionType] ([transTypeID], [typeName]) VALUES (2, N'Deposit')
+GO
+INSERT [dbo].[TransactionType] ([transTypeID], [typeName]) VALUES (3, N'Scheduled')
+GO
+INSERT [dbo].[TransactionType] ([transTypeID], [typeName]) VALUES (4, N'Failed')
+GO
+
+INSERT [dbo].[Transaction] ([transId], [transDate], [cardNum], [amount], [transType]) VALUES (1, CAST(N'2017-02-02' AS Date), N'6569', 500,1)
+GO
+INSERT [dbo].[Transaction] ([transId], [transDate], [cardNum], [amount], [transType]) VALUES (2, CAST(N'2018-02-03' AS Date), N'3436', 3000,3)
+GO
+INSERT [dbo].[Transaction] ([transId], [transDate], [cardNum], [amount], [transType]) VALUES (3, CAST(N'2017-05-06' AS Date), N'6566', 2500,2)
+GO
+INSERT [dbo].[Transaction] ([transId], [transDate], [cardNum], [amount], [transType]) VALUES (4, CAST(N'2016-09-09' AS Date), N'6566', 2000,1)
+GO
+INSERT [dbo].[Transaction] ([transId], [transDate], [cardNum], [amount], [transType]) VALUES (5, CAST(N'2015-02-10' AS Date), N'3336', 6000,4)
+GO
+
+
+Select * from UserType
+Select * from [User]
+Select * from UserCard
+Select * from [Card]
+Select * from CardType
+Select * from [Transaction]
+Select * from TransactionType
+
+-- Task 1
+Select * from [Card] c
+where c.cardnum NOT IN(Select t.cardnum from [Transaction] t)
+
+-- Task 2
+Select u.name from [user] u
+where userId in 
+(Select userid from UserCard join [card] c on UserCard.cardnum = c.cardNum where balance IN (select max(c1.balance) from [card] c1))
+
+-- Task 3
+Select u.name ,(Select ut.name from UserType ut where u.userId = ut.userTypeId) 
+from [User] u
+
+-- Task 4
+Select *
+from [USERCARD] where cardnum IN (Select t.cardnum from [Transaction] t where transtype IN
+(Select tt.transtypeid from [TransactionType] tt where tt.typename = 'Failed')) 
+
+-- Task 5
+Select uc.userid from [usercard] uc
+where uc.cardnum IN 
+(Select c.cardnum from [card] c where c.cardnum IN 
+(Select top 1 cardnum from [Transaction] t group by t.cardnum order by count(cardnum) desc))
+
+-- Task 6
+select UserType.name from UserType join [user] on UserType.UserTypeId = [user].UserId 
+join UserCard on [user].Userid = UserCard.UserID 
+join [card] on Usercard.cardNum = [Card].cardNum 
+join cardtype on [card].cardTypeID = cardtype.cardTypeID 
+where cardtype.name = 'Credit';
+
+-- Task 7
+Select name, count(*) from cardtype 
+group by name
+order by count(*) desc
+
+-- Task 8
+Select u.name from [user] u
+where userId in 
+(Select userid from UserCard join [card] c on UserCard.cardnum = c.cardNum where balance IN (select top 1(c1.balance) from [card] c1))
+
+-- Task 9
+Select ut.* 
+from usertype ut join [user] u on ut.usertypeid = u.userid
+join usercard uc on u.userid = uc.userid 
+join [card] c on uc.cardnum = c.cardnum
+join [transaction] t on c.cardnum = t.cardnum
+join [transactiontype] tt on t.transtype = tt.transtypeid
+where tt.typename != 'Withdrawl'
